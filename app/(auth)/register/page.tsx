96188351e268e5ec583d2c524fe9f4a282358de9
Fernando Alcázar Malfavón <4maferal@gmail.com>
2025-01-22 22:17:41 -0600

    Feat: update register component

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -2,16 +2,22 @@ import Image from "next/image";
 import AuthForm from "../_component/Form/Form";
 import AuthBtn from "../_component/AuthBtn/AuthBtn";
 import './style.css'
+import AuthPassword from "../_component/AuthPassword/AuthPassword";
 
 export default function Register() {
   return<>
     <Image src='/Floating-Credit-Cards-Mockup.png' alt='Floating credit cards' width={600} height={1200} />
-    <AuthForm action={'function'} title="Bienvenido" text="Para empezar, ingresá un DNI o un  Cuit" className="dni-cuit">
+    <AuthForm action={'function'} title="Bienvenido" text="Ingresa tú correo electrónico" className="register">
       <label>
-        <span>El documento determinará quien será titular de la cuenta</span>
-        <input type="text" name="id" />
+        <span>Correo electrónico</span>
+        <input type="email" name="email" placeholder="ejemplo@gmail.com" />
       </label>
-      <AuthBtn>CONTINUAR</AuthBtn>
+      <AuthPassword leyend="Contraseña" />
+      <AuthPassword leyend="Repetir Contraseña" name="confirm" />
+      <menu>
+        <li><AuthBtn>Registrarse</AuthBtn></li>
+        <li><AuthBtn mod="inherit" href="/login">Iniciar Sesión</AuthBtn></li>
+      </menu>
     </AuthForm>
   </>
 }
