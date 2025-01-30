bbb2fb7227eb8dd12098a56a09f26c1c1afa2e31
Fernando Alcázar Malfavón <4maferal@gmail.com>
2025-01-29 20:29:22 -0600

    fix: remove nav of landing from root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Nav from "@/components/Nav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +25,6 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Nav />
         {children}
       </body>
     </html>
