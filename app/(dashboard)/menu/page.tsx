1c5445800a3d27eb4954acb5ed8d6a8919a088e4
Fernando Alcázar Malfavón <4maferal@gmail.com>
2025-01-30 20:03:01 -0600

    Feat: add MenuNav component

--- app/(dashboard)/menu/page.tsx
+++ app/(dashboard)/menu/page.tsx
@@ -1,10 +1,14 @@
 import Balance from "./_components/Balance/Balance";
+import MenuNav from "./_components/MenuNav/MenuNav";
 import './Menu.css'
 
 export default function Menu() {
   return <>
     <main className="menu">
       <Balance />
+      <nav>
+        <MenuNav />
+      </nav>
     </main>
   </>
 }
