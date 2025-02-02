44612507389fedba8b305e59781714787e0b49dd
Fernando Alcázar Malfavón <4maferal@gmail.com>
2025-02-02 15:51:40 -0600

    feat: comunicate register

---
+++ app/(auth)/_lib/api.ts
@@ -0,0 +1,22 @@
+import { signIn, SignInResponse } from "next-auth/react";
+
+type Register = Record<'email' | 'password', FormDataEntryValue>
+
+const postOpt = {
+  method: 'POST',
+  headers: {
+    'Content-type': 'aplication/json',
+  },
+};
+
+function confirm(res: Response) {
+  if (res.ok) return res.json()
+  return Promise.reject(`Error: ${res.status}`)
+}
+
+export function register(form: Register)  {
+  const slug = 'api/auth/register';
+  const body = JSON.stringify(form);
+
+  return fetch(slug, { ...postOpt, body }).then(confirm)
+}
