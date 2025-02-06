80a1d04c5e2c664c711c84c14ed1ec82f6fabf05
LissbethE <lissbethescobar09@gmail.com>
2025-02-05 19:18:54 -0600

    Added Activity Component

--- app/(dashboard)/menu/page.tsx
+++ app/(dashboard)/menu/page.tsx
@@ -1,20 +1,25 @@
+import Activity from "@/components/Activity";
 import AddCard from "./_components/AddCard/AddCard";
 import Balance from "./_components/Balance/Balance";
 import Market from "./_components/Market/Market";
 import MenuNav from "./_components/MenuNav/MenuNav";
 import Statics from "./_components/Statics/Statics";
-import './Menu.css'
+import "./Menu.css";
 
 export default function Menu() {
-  return <>
-    <main className="menu">
-      <Balance />
-      <nav>
-        <MenuNav />
-      </nav>
-      <Statics />
-      <AddCard />
-      <Market />
-    </main>
-  </>
+  return (
+    <>
+      <main className="menu">
+        <Balance />
+        <nav>
+          <MenuNav />
+        </nav>
+        <Statics />
+        <AddCard />
+        <Market />
+
+        <Activity />
+      </main>
+    </>
+  );
 }
--- components/Activity.tsx
+++ components/Activity.tsx
@@ -1,45 +1,79 @@
 import React from "react";
 import "../style/activity.css";
+import Image from "next/image";
 
 const dataFake = [
   {
-    img: "/amazon.png",
+    img: "/amazon.jpg",
     name: "Amazon",
     date: "May 24, 2022",
     amount: "- $103.56",
   },
 
   {
-    img: "/mcdonalds.png",
+    img: "/mcdonalds.jpg",
     name: "Mcdonalds",
     date: "May 12, 2022",
     amount: "- $34.78",
   },
 
   {
-    img: "/apple.png",
+    img: "/apple.jpg",
     name: "Apple",
     date: "May 8, 2022",
     amount: "- $1,000.97",
   },
 
   {
-    img: "/apple.png",
+    img: "/starbucks.jpg",
     name: "Starbucks",
     date: "May 8 , 2022",
     amount: "- $13.67",
+  },
+
+  {
+    img: "/mastercard.jpg",
+    name: "Mastercard",
+    date: "May 6, 2022",
+    amount: "- $1,000.00",
+  },
+
+  {
+    img: "/freelancer.jpg",
+    name: "Freelancer",
+    date: "May 6, 2022",
+    amount: "+ $3300.00",
   },
 ];
 
 export default function Activity() {
   return (
     <div className="activity">
-      <div></div>
+      <div className="activity__days">
+        <div>
+          <h2>$0000000</h2>
+          <p>Mi Día A Día</p>
+        </div>
+      </div>
 
-      <div>
+      <div className="activity__container">
         <p>Tu última actividad</p>
         <p>Ver todo</p>
-        <ul></ul>
+
+        <ul className="activity__list">
+          {dataFake.map((data) => (
+            <li key={data.name} className="activity__item">
+              <Image src={`${data.img}`} alt="" width={40} height={40} />
+
+              <div className="activity__texts">
+                <h3>{data.name}</h3>
+                <p>{data.date}</p>
+              </div>
+
+              <p className="activity__amount">{data.amount}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
